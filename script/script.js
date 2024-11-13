f4a83ceaadf182bc100a0926bb84ddde32d9e142
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2024-11-13 18:31:53 +0100

    fixed check sistem

--- script/script.js
+++ script/script.js
@@ -13,62 +13,77 @@ const timerDiv = document.getElementById("time");
 // costanti js
 let numCounter = 5;
 let seconds = 29;
+// array con i numeri
 const numbersArray = [];
 const guessArray = [];
 let rightGuess = 0;
 
-addButton.addEventListener('click', function(){
+addButton.addEventListener('click', function () {
     numCounter += 1
     return numCounter;
 })
 
-startButton.addEventListener('click', function(){
-    setTimeout(function (){
-            guessSection.classList.remove("d-none");
-            numberSection.classList.add("d-none");
-            for(let i = 0 ; i < numCounter ; i++){
-                let inputGuess = document.createElement("input");
-                let typeInput = document.createAttribute("type");
-                inputGuess.getAttribute(typeInput);
-                inputGuess.classList.add("inputGuess")
-                inputGuess.id = i;
-                inputGuess.type = "number";
-                inputGuess.name = "numero"
-                inputSection.appendChild(inputGuess);
-            }
-        }, 31000)
-        setInterval(function(){
-            if(seconds >= 0){
-                timerDiv.innerHTML = seconds--;        
-            }else{
-                timerDiv.classList.add("d-none")
-            }
-            if(seconds <= 2){
-                timerDiv.classList.add("text-danger");
-            }
-        }, 1000);
-        numContainer.innerHTML = "";
-        for(let i = 0; i < numCounter ; i++){
-            const number = document.createElement("div");
-            number.innerHTML = randomize(1, 100);
-            numContainer.appendChild(number);
-            number.classList.add("m-4", "fw-bold", "fs-4")
-            numbersArray.push(number.innerHTML);
+startButton.addEventListener('click', function () {
+
+    // Timer e creazione input
+    setTimeout(function () {
+        guessSection.classList.remove("d-none");
+        numberSection.classList.add("d-none");
+
+        for (let i = 0; i < numCounter; i++) {
+            let inputGuess = document.createElement("input");
+            let typeInput = document.createAttribute("type");
+
+            inputGuess.getAttribute(typeInput);
+            inputGuess.classList.add("inputGuess")
+
+            inputGuess.id = i;
+            inputGuess.type = "number";
+            inputGuess.name = "numero"
+
+            inputSection.appendChild(inputGuess);
         }
+    }, 31000);
+
+    // Count Down
+    setInterval(function () {
+        if (seconds >= 0) {
+            timerDiv.innerHTML = seconds--;
+        } else {
+            timerDiv.classList.add("d-none")
+        }
+        if (seconds <= 2) {
+            timerDiv.classList.add("text-danger");
+        }
+    }, 1000);
+
+    // Creazione numeri random
+    numContainer.innerHTML = "";
+    for (let i = 0; i < numCounter; i++) {
+        const number = document.createElement("div");
+        number.innerHTML = randomize(1, 100);
+        numContainer.appendChild(number);
+        number.classList.add("m-4", "fw-bold", "fs-4")
+        numbersArray.push(number.innerHTML);
+        console.log('create ', numbersArray);
+    }
 })
 
-    // DOPO 30 SECONDI
-checkButton.addEventListener('click', function(){
-    for(let i = 1 ; i < numCounter ; i++){
-        guessArray.push(inputGuess.value[i])
-        if(numbersArray.includes(guessArray[i])){
-           rightGuess += 1 
+
+// Al click del bottone per verificare
+checkButton.addEventListener('click', function () {
+    console.log('click ', numbersArray);
+    const inputGuess = document.querySelectorAll('.inputGuess');
+    console.log('click ', inputGuess);
+
+    for (let i = 0; i < inputGuess.length; i++) {
+        if (numbersArray.includes(inputGuess[i].value)) {
+            rightGuess += 1
         }
     }
+
     const result = document.createElement("div")
     result.innerHTML = `Bravo, hai indovinato ${rightGuess} numeri su ${numCounter}!`
     result.classList.add('result', "fs-3", "fw-bold")
     resultSection.appendChild(result);
-        
-
 })
